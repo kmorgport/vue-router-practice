24346579db298831c084fbe5204827799b782d5a
kmorgport <kmorgan0057@gmail.com>
2021-05-13 11:58:54 -0500

    path error handling

--- src/main.js
+++ src/main.js
@@ -5,15 +5,19 @@ import App from './App.vue'
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {path: '/', redirect: '/teams'},
+        //with alias the url does not change
         { path: '/teams', component: TeamsList }, //when /teams is loaded a certain component is loaded
         { path: '/users', component: UsersList } ,
         // dynamic parameters should be passed into component as props rather than just $route property
         // :teamId will be passed into the named component as a prop
-        { path: '/teams/:teamId', component: TeamMembers, props:true}
+        { path: '/teams/:teamId', component: TeamMembers, props:true},
+        {path: '/:notFound(.*)', component: NotFound}
     ],
     linkActiveClass: 'active'
 });
